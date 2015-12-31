3ca6f16a80cf5be7e9c4e35396d912fa11dd9b10
ksharifbd <ks@inov.io>
2015-12-31 19:19:28 +0600

    typescript linter added

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,31 @@
-var gulp = require('gulp'),
-    tsc = require('gulp-typescript');
+var gulp = require('gulp');
+var tsc = require('gulp-typescript');
+var tslint = require('gulp-tslint');
 
 // Compile typescript sources
 gulp.task('tsc', function() {  
     gulp.src(['source/*.ts'])
-        .pipe(tsc({module: 'commonjs'}))
+        .pipe(tsc({
+        module: 'commonjs',
+        noImplicitAny: false,
+        target: 'ES5',
+        removeComments: false,
+        noEmitOnError: false,
+        preserveConstEnums: true
+        }))
         .js
         .pipe(gulp.dest('js/'));
 });
 
+// linting
+gulp.task('tslint', function(){
+    gulp.src('source/*.ts')
+    .pipe(tslint())
+    .pipe(tslint.report('full'));
+});
+
 // watching files
-gulp.task('watch', function() {  
+gulp.task('watch', function(){  
     gulp.watch('source/*.ts', ['tsc']);
 });
 
