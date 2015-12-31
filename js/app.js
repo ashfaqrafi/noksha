fa77876988131a1ffeb6a2dfda6fab296b7aefe3
ksharifbd <ks@inov.io>
2015-12-31 22:10:56 +0600

    browsersync and sourcemaps added

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function greeter(person) {
+    return 'Hello, ' + person;
+}
+var user = 'Jane User';
+document.getElementById('test').innerHTML = greeter(user);
+
+//# sourceMappingURL=smaps/app.js.map
